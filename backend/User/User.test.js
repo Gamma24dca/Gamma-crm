08b0e8bae141683bf3f6c1ca8c24b816d331713d
Davcode1234 <dpietruszewski12@gmail.com>
2023-10-16 12:59:26 +0200

    Init project

---
+++ backend/User/User.test.js
@@ -0,0 +1,28 @@
+import mongoose from "mongoose";
+
+export const UserModel = mongoose.model(
+  "User",
+  new mongoose.Schema({
+    name: {
+      type: String,
+      required: true,
+    },
+    lastname: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    job: {
+      type: String,
+      required: true,
+    },
+  })
+);
