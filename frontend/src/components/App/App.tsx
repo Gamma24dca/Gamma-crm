0d77a717b57da2b7181350fae98fe757b726591a
Davcode1234 <dpietruszewski12@gmail.com>
2023-10-17 09:30:40 +0200

    Add cached frontend folder

---
+++ frontend/src/components/App/App.tsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignIn from "../SignIn/SignIn";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignIn />}></Route>
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/SignIn/SignIn.tsx
@@ -0,0 +1,11 @@
+import styles from "./SignIn.module.css";
+
+function SignIn() {
+  return (
+    <>
+      <h1 className={styles.testHeader}>SignIn</h1>
+    </>
+  );
+}
+
+export default SignIn;
