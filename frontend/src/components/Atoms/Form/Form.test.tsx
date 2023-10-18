74b349bd48dd1b3f23412b878393228e83cfce5d
Davcode1234 <dpietruszewski12@gmail.com>
2023-10-18 14:35:26 +0200

    Add tests for form component and create input component

--- frontend/src/components/Atoms/Form/Form.test.tsx
+++ frontend/src/components/Atoms/Form/Form.test.tsx
@@ -2,15 +2,24 @@ import "@testing-library/jest-dom";
 import { render } from "@testing-library/react";
 import Form from "./Form.tsx";
 
-test("demo", () => {
-  expect(true).toBe(true);
-});
-
-test("Renders the main page", () => {
-  render(
+it("Renders to the page ", () => {
+  const { getByTestId } = render(
     <Form>
       <input />
     </Form>
   );
-  expect(true).toBeTruthy();
+
+  const form = getByTestId("form");
+  expect(form).toBeVisible();
+});
+
+it("Accept children input", () => {
+  const { getByTestId } = render(
+    <Form>
+      <input />
+    </Form>
+  );
+
+  const form = getByTestId("form");
+  expect(form).toContainHTML("input");
 });
