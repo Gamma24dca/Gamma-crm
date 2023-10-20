bd3c808e3a562fc470f7e47a58dbb94d473b921f
Davcode1234 <dpietruszewski12@gmail.com>
2023-10-20 10:48:59 +0200

    Create sign in UI

--- frontend/src/components/Organisms/SignIn/SignIn.tsx
+++ frontend/src/components/Organisms/SignIn/SignIn.tsx
@@ -1,11 +1,27 @@
+import FormContainer from '../../Atoms/FormContainer/FormContainer';
+import Form from '../../Atoms/Form/Form';
 import Input from '../../Atoms/Input/Input';
 import styles from './SignIn.module.css';
+import FormControl from '../../Atoms/FormControl/FormControl';
+import Label from '../../Atoms/Label/Label';
 
 function SignIn() {
   return (
     <>
-      <h1 className={styles.testHeader}>SignIn</h1>
-      <Input id="password" type="password" name="password" />
+      <img src="./orange.png" alt="logo gamma" className={styles.logo} />
+      <FormContainer>
+        <Form>
+          <h1 className={styles.testHeader}>Zaloguj się</h1>
+          <FormControl>
+            <Label htmlFor="email" labelContent="Email" />
+            <Input id="email" type="email" name="email" />
+          </FormControl>
+          <FormControl>
+            <Label htmlFor="password" labelContent="Hasło" />
+            <Input id="password" type="password" name="password" />
+          </FormControl>
+        </Form>
+      </FormContainer>
     </>
   );
 }
