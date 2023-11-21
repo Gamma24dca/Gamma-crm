da422ca9804678a9b731803d67017395e1b3cc8f
Davcode1234 <dpietruszewski12@gmail.com>
2023-11-21 14:40:25 +0100

    Fix error

--- frontend/src/components/Atoms/Form/Form.tsx
+++ frontend/src/components/Atoms/Form/Form.tsx
@@ -2,11 +2,12 @@ import styles from './Form.module.css';
 
 type FormProps = {
   children: string | JSX.Element | JSX.Element[];
+  onSubmit: (e?: React.FormEvent<HTMLFormElement>) => void;
 };
 
-function Form({ children, ...restProps }: FormProps) {
+function Form({ children, onSubmit }: FormProps) {
   return (
-    <form data-testid="form" className={styles.form} {...restProps}>
+    <form data-testid="form" className={styles.form} onSubmit={onSubmit}>
       {children}
     </form>
   );
