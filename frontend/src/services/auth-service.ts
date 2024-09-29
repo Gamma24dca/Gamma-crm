427c37d173acf5b42f7b0421ee5b6fb2ae7300da
Dawid Pietruszewski <dpietruszewski12@gmail.com>
2024-09-29 16:31:46 +0200

    hardcode auth request urls

--- frontend/src/services/auth-service.ts
+++ frontend/src/services/auth-service.ts
@@ -30,13 +30,16 @@ export async function signUp({
     img,
   };
 
-  const response = await fetch('/api/auth/signup', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(formInfo),
-  });
+  const response = await fetch(
+    'https://gamma-crm.onrender.com/api/auth/signup',
+    {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formInfo),
+    }
+  );
 
   if (!response.ok) {
     throw new Error(`${response.status} ${response.statusText}`);
@@ -72,9 +75,12 @@ export async function signIn({ email, password }: SignInProps): Promise<void> {
 }
 
 export async function signOut(): Promise<void> {
-  const response = await fetch('/api/auth/signout', {
-    method: 'POST',
-  });
+  const response = await fetch(
+    'https://gamma-crm.onrender.com/api/auth/signout',
+    {
+      method: 'POST',
+    }
+  );
 
   if (!response.ok) {
     throw new Error(`${response.status} ${response.statusText}`);
