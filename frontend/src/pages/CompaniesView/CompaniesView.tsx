847660702f550c2f539c4c01063e6f2d4b65e455
Davcode1234 <dpietruszewski12@gmail.com>
2024-10-09 15:11:29 +0200

    fetch and display companies

--- frontend/src/pages/CompaniesView/CompaniesView.tsx
+++ frontend/src/pages/CompaniesView/CompaniesView.tsx
@@ -1,9 +1,22 @@
+import { useEffect, useState } from 'react';
 import styles from './CompaniesView.module.css';
 import ViewContainer from '../../components/Atoms/ViewContainer/ViewContainer';
 import ControlBar from '../../components/Atoms/ControlBar/ControlBar';
 import ListContainer from '../../components/Atoms/ListContainer/ListContainer';
+import {
+  CompaniesType,
+  getAllCompanies,
+} from '../../services/companies-service';
 
 function CompaniesView() {
+  const [companies, setCompanies] = useState<CompaniesType[] | undefined>([]);
+
+  useEffect(() => {
+    getAllCompanies().then((allCompanies) => {
+      setCompanies(allCompanies);
+    });
+  }, []);
+
   return (
     <>
       <ControlBar>
@@ -17,9 +30,23 @@ function CompaniesView() {
 
       <ViewContainer>
         <ListContainer>
-          <div className={styles.test}>1</div>
-          <div>2</div>
-          <div>3</div>
+          {companies && companies.length > 0 ? (
+            <>
+              {companies.map((company) => {
+                return (
+                  <div key={company._id} className={styles.test}>
+                    <p>{company.name}</p>
+                    <p>{company.phone}</p>
+                    <p>{company.mail}</p>
+                    <p>{company.website}</p>
+                    <p>{company.activeTasks}</p>
+                  </div>
+                );
+              })}
+            </>
+          ) : (
+            <p>loading</p>
+          )}
         </ListContainer>
       </ViewContainer>
     </>
