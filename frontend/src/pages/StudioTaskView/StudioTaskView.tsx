a691d6eac1d6fb0ead12c36b40f8bb836227953d
Dawid Pietruszewski <dpietruszewski12@gmail.com>
2024-11-12 19:19:31 +0100

    Display tasks in columns based on status

--- frontend/src/pages/StudioTaskView/StudioTaskView.tsx
+++ frontend/src/pages/StudioTaskView/StudioTaskView.tsx
@@ -7,154 +7,154 @@ import ViewContainer from '../../components/Atoms/ViewContainer/ViewContainer';
 // import InfoBar from '../../components/Organisms/InfoBar/InfoBar';
 import styles from './StudioTaskView.module.css';
 
-// const mockedTasks = [
-//   {
-//     title: 'Leaflet Design',
-//     client: 'Shell',
-//     clientPerson: 'Anna Kowalska',
-//     status: 'W trakcie',
-//     author: [
-//       {
-//         _id: '65608b6b1ad0aa5b987a8452',
-//         name: 'Marek',
-//         lastname: 'Kowal',
-//         email: 'marek.kowal@gamma24.pl',
-//         phone: 512345678,
-//         job: 'Project Manager',
-//         img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038449/AboutPage/Gamma_Marek-min.jpg',
-//       },
-//     ],
-//     TaskType: 'Marketing',
-//     participants: [
-//       {
-//         _id: '67059fb0dcfe272f6b1f1bb3',
-//         name: 'Joanna',
-//         lastname: 'Nowak',
-//         email: 'joanna.nowak@gamma24.pl',
-//         phone: 501234567,
-//         job: 'Designer',
-//         img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038450/AboutPage/Gamma_Joanna-min.jpg',
-//       },
-//       {
-//         _id: '655f423bf7ce6ff8c9b4f308',
-//         name: 'Adam',
-//         lastname: 'Wisniewski',
-//         email: 'adam.wisniewski@gamma24.pl',
-//         phone: 502345678,
-//         job: 'Content Specialist',
-//         img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038451/AboutPage/Gamma_Adam-min.jpg',
-//       },
-//     ],
-//     description: 'Design a promotional leaflet for Shell’s new product line.',
-//     subtasks: [
-//       {
-//         content: 'Finalize layout design',
-//       },
-//       {
-//         content: 'Send for client review',
-//       },
-//     ],
-//     deadline: '25.11.2024',
-//   },
-//   {
-//     title: 'Annual Report 2024',
-//     client: 'Goodyear',
-//     clientPerson: 'Piotr Nowakowski',
-//     status: 'Do zrobienia',
-//     author: [
-//       {
-//         _id: '65608b6b1ad0aa5b987a8453',
-//         name: 'Monika',
-//         lastname: 'Lis',
-//         email: 'monika.lis@gamma24.pl',
-//         phone: 513456789,
-//         job: 'Coordinator',
-//         img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038452/AboutPage/Gamma_Monika-min.jpg',
-//       },
-//     ],
-//     TaskType: 'Corporate',
-//     participants: [
-//       {
-//         _id: '67059fb0dcfe272f6b1f1bb4',
-//         name: 'Jan',
-//         lastname: 'Bialy',
-//         email: 'jan.bialy@gamma24.pl',
-//         phone: 503456789,
-//         job: 'Data Analyst',
-//         img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038453/AboutPage/Gamma_Jan-min.jpg',
-//       },
-//       {
-//         _id: '655f423bf7ce6ff8c9b4f309',
-//         name: 'Kasia',
-//         lastname: 'Nowak',
-//         email: 'kasia.nowak@gamma24.pl',
-//         phone: 504567890,
-//         job: 'Editor',
-//         img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038454/AboutPage/Gamma_Kasia-min.jpg',
-//       },
-//     ],
-//     description:
-//       'Compile and design the annual report for Goodyear’s fiscal year 2024.',
-//     subtasks: [
-//       {
-//         content: 'Collect financial data',
-//       },
-//       {
-//         content: 'Prepare visualizations and charts',
-//       },
-//     ],
-//     deadline: '15.12.2024',
-//   },
-//   {
-//     title: 'Website Redesign',
-//     client: 'Axa',
-//     clientPerson: 'Elżbieta Kamińska',
-//     status: 'Wysłane',
-//     author: [
-//       {
-//         _id: '65608b6b1ad0aa5b987a8454',
-//         name: 'Dawid',
-//         lastname: 'Cichy',
-//         email: 'dawid.cichy@gamma24.pl',
-//         phone: 505678901,
-//         job: 'Lead Developer',
-//         img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038455/AboutPage/Gamma_Dawid-min.jpg',
-//       },
-//     ],
-//     TaskType: 'Web Development',
-//     participants: [
-//       {
-//         _id: '67059fb0dcfe272f6b1f1bb5',
-//         name: 'Paulina',
-//         lastname: 'Radzka',
-//         email: 'paulina.radzka@gamma24.pl',
-//         phone: 506789012,
-//         job: 'Frontend Developer',
-//         img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038456/AboutPage/Gamma_Paulina-min.jpg',
-//       },
-//       {
-//         _id: '655f423bf7ce6ff8c9b4f310',
-//         name: 'Tomasz',
-//         lastname: 'Nowicki',
-//         email: 'tomasz.nowicki@gamma24.pl',
-//         phone: 507890123,
-//         job: 'UX Designer',
-//         img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038457/AboutPage/Gamma_Tomasz-min.jpg',
-//       },
-//     ],
-//     description:
-//       'Complete overhaul of the Axa corporate website to improve UX and accessibility.',
-//     subtasks: [
-//       {
-//         content: 'Finalize wireframes',
-//       },
-//       {
-//         content: 'Implement responsive design',
-//       },
-//     ],
-//     deadline: '30.10.2024',
-//   },
-// ];
+const mockedTasks = [
+  {
+    title: 'Leaflet Design',
+    client: 'Shell',
+    clientPerson: 'Anna Kowalska',
+    status: 'W trakcie',
+    author: [
+      {
+        _id: '65608b6b1ad0aa5b987a8452',
+        name: 'Marek',
+        lastname: 'Kowal',
+        email: 'marek.kowal@gamma24.pl',
+        phone: 512345678,
+        job: 'Project Manager',
+        img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038449/AboutPage/Gamma_Marek-min.jpg',
+      },
+    ],
+    TaskType: 'Marketing',
+    participants: [
+      {
+        _id: '67059fb0dcfe272f6b1f1bb3',
+        name: 'Joanna',
+        lastname: 'Nowak',
+        email: 'joanna.nowak@gamma24.pl',
+        phone: 501234567,
+        job: 'Designer',
+        img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038450/AboutPage/Gamma_Joanna-min.jpg',
+      },
+      {
+        _id: '655f423bf7ce6ff8c9b4f308',
+        name: 'Adam',
+        lastname: 'Wisniewski',
+        email: 'adam.wisniewski@gamma24.pl',
+        phone: 502345678,
+        job: 'Content Specialist',
+        img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038451/AboutPage/Gamma_Adam-min.jpg',
+      },
+    ],
+    description: 'Design a promotional leaflet for Shell’s new product line.',
+    subtasks: [
+      {
+        content: 'Finalize layout design',
+      },
+      {
+        content: 'Send for client review',
+      },
+    ],
+    deadline: '25.11.2024',
+  },
+  {
+    title: 'Annual Report 2024',
+    client: 'Goodyear',
+    clientPerson: 'Piotr Nowakowski',
+    status: 'Do zrobienia',
+    author: [
+      {
+        _id: '65608b6b1ad0aa5b987a8453',
+        name: 'Monika',
+        lastname: 'Lis',
+        email: 'monika.lis@gamma24.pl',
+        phone: 513456789,
+        job: 'Coordinator',
+        img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038452/AboutPage/Gamma_Monika-min.jpg',
+      },
+    ],
+    TaskType: 'Corporate',
+    participants: [
+      {
+        _id: '67059fb0dcfe272f6b1f1bb4',
+        name: 'Jan',
+        lastname: 'Bialy',
+        email: 'jan.bialy@gamma24.pl',
+        phone: 503456789,
+        job: 'Data Analyst',
+        img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038453/AboutPage/Gamma_Jan-min.jpg',
+      },
+      {
+        _id: '655f423bf7ce6ff8c9b4f309',
+        name: 'Kasia',
+        lastname: 'Nowak',
+        email: 'kasia.nowak@gamma24.pl',
+        phone: 504567890,
+        job: 'Editor',
+        img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038454/AboutPage/Gamma_Kasia-min.jpg',
+      },
+    ],
+    description:
+      'Compile and design the annual report for Goodyear’s fiscal year 2024.',
+    subtasks: [
+      {
+        content: 'Collect financial data',
+      },
+      {
+        content: 'Prepare visualizations and charts',
+      },
+    ],
+    deadline: '15.12.2024',
+  },
+  {
+    title: 'Website Redesign',
+    client: 'Axa',
+    clientPerson: 'Elżbieta Kamińska',
+    status: 'Wysłane',
+    author: [
+      {
+        _id: '65608b6b1ad0aa5b987a8454',
+        name: 'Dawid',
+        lastname: 'Cichy',
+        email: 'dawid.cichy@gamma24.pl',
+        phone: 505678901,
+        job: 'Lead Developer',
+        img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038455/AboutPage/Gamma_Dawid-min.jpg',
+      },
+    ],
+    TaskType: 'Web Development',
+    participants: [
+      {
+        _id: '67059fb0dcfe272f6b1f1bb5',
+        name: 'Paulina',
+        lastname: 'Radzka',
+        email: 'paulina.radzka@gamma24.pl',
+        phone: 506789012,
+        job: 'Frontend Developer',
+        img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038456/AboutPage/Gamma_Paulina-min.jpg',
+      },
+      {
+        _id: '655f423bf7ce6ff8c9b4f310',
+        name: 'Tomasz',
+        lastname: 'Nowicki',
+        email: 'tomasz.nowicki@gamma24.pl',
+        phone: 507890123,
+        job: 'UX Designer',
+        img: 'https://res.cloudinary.com/dpktrptfr/image/upload/v1679038457/AboutPage/Gamma_Tomasz-min.jpg',
+      },
+    ],
+    description:
+      'Complete overhaul of the Axa corporate website to improve UX and accessibility.',
+    subtasks: [
+      {
+        content: 'Finalize wireframes',
+      },
+      {
+        content: 'Implement responsive design',
+      },
+    ],
+    deadline: '30.10.2024',
+  },
+];
 
 const colums = [
   {
@@ -189,62 +189,25 @@ function StudioTaskView() {
 
       <ViewContainer>
         <div className={styles.columnsWrapper}>
-          {/* <div className={styles.taskColumnContainer}>
-            <div className={styles.columnTitleWrapper}>
-              <div className={styles.columnTitleMark} />
-              <h4>Na później</h4>
-            </div>
-            <div className={styles.taskColumn}>
-              <div className={styles.task}>
-                <p>task</p>
-              </div>
-            </div>
-          </div>
-
-          <div className={styles.taskColumnContainer}>
-            <div className={styles.columnTitleWrapper}>
-              <div className={styles.columnTitleMarkToDo} />
-              <h4>Do zrobienia</h4>
-            </div>
-            <div className={styles.taskColumn}>
-              <div className={styles.task}>
-                <p>task</p>
-              </div>
-            </div>
-          </div>
-          <div className={styles.taskColumnContainer}>
-            <div className={styles.columnTitleWrapper}>
-              <div className={styles.columnTitleMarkInProgress} />
-              <h4>W trakcie</h4>
-            </div>
-            <div className={styles.taskColumn}>
-              <div className={styles.task}>
-                <p>task</p>
-              </div>
-            </div>
-          </div>
-          <div className={styles.taskColumnContainer}>
-            <div className={styles.columnTitleWrapper}>
-              <div className={styles.columnTitleMarkSent} />
-              <h4>Wysłane</h4>
-            </div>
-            <div className={styles.taskColumn}>
-              <div className={styles.task}>
-                <p>task</p>
-              </div>
-            </div>
-          </div> */}
           {colums.map((column) => {
+            console.log(column.class);
             return (
               <div className={styles.taskColumnContainer} key={column.class}>
                 <div className={styles.columnTitleWrapper}>
-                  <div className={styles.columnTitleMarkSent} />
+                  <div className={` ${styles[`${column.class}`]}`} />
                   <h4>{column.title}</h4>
                 </div>
                 <div className={styles.taskColumn}>
-                  <div className={styles.task}>
-                    <p>task</p>
-                  </div>
+                  {mockedTasks.map((task) => {
+                    return (
+                      task.status === column.title && (
+                        <div className={styles.task}>
+                          <p>{task.client}</p>
+                          <p>{task.description}</p>
+                        </div>
+                      )
+                    );
+                  })}
                 </div>
               </div>
             );
