6e8f8b59111170ed80b372fe3b75ab6e3976a31e
Davcode1234 <dpietruszewski12@gmail.com>
2025-01-24 11:25:40 +0100

    Add uuid to creating subtask and add update subtask API route

--- backend/src/StudioTask/StudioTask.controller.ts
+++ backend/src/StudioTask/StudioTask.controller.ts
@@ -1,3 +1,4 @@
+import { randomUUID } from 'crypto';
 import { StudioTaskModel } from './StudioTask.model';
 
 export const StudioTaskController = {
@@ -25,11 +26,22 @@ export const StudioTaskController = {
   },
   async addSubtask(taskId, subtask) {
     const task = await StudioTaskController.getStudioTask(taskId);
-    task.subtasks.push(subtask);
+    task.subtasks.push({ ...subtask, _id: randomUUID() });
     await StudioTaskController.updateStudioTask(taskId, task);
     const updatedStudioTask = await StudioTaskController.getStudioTask(taskId);
     return updatedStudioTask.subtasks;
   },
+  async updateSubtask(taskId, subtaskId, subtask) {
+    const task = await StudioTaskController.getStudioTask(taskId);
+    task.subtasks = task.subtasks.map((obj) => {
+      return obj._id === subtaskId ? { ...obj, ...subtask } : obj;
+    });
+
+    await StudioTaskController.updateStudioTask(taskId, task);
+    const updatedStudioTask = await StudioTaskController.getStudioTask(taskId);
+    return updatedStudioTask.subtasks;
+  },
+
   async deleteSubtask(taskId, subtaskId) {
     const task = await StudioTaskController.getStudioTask(taskId);
     console.log(task);
