11b9c4cfac3495e888b575b958918f08379f6ad2
Davcode1234 <dpietruszewski12@gmail.com>
2025-01-27 15:21:15 +0100

    Separate update task logic to custom hooks

---
+++ frontend/src/hooks/useStudioTaskUpdate.ts
@@ -0,0 +1,207 @@
+import { useState } from 'react';
+import { archiveStudioTask } from '../services/archived-studio-tasks-service';
+import {
+  deleteTask,
+  getAllStudioTasks,
+  UpdateStudioTask,
+  StudioTaskTypes,
+} from '../services/studio-tasks-service';
+import useStudioTasksContext from './Context/useStudioTasksContext';
+import useUsersContext from './Context/useUsersContext';
+import useCompaniesContext from './Context/useCompaniesContext';
+
+const useStudioTaskUpdate = (task, closeModal) => {
+  const [isEditing, setIsEditing] = useState(false);
+  const [isSelectOpen, setIsSelectOpen] = useState(false);
+  const [isUserAssigned, setIsUserAssigned] = useState(false);
+  const [formValue, setFormValue] = useState<StudioTaskTypes>(task);
+  const [isMemberChangeLoading, setIsMemberChangeLoading] = useState({
+    userName: '',
+    isLoading: false,
+    loadPlace: '',
+  });
+  const { users } = useUsersContext();
+  const { companies } = useCompaniesContext();
+
+  const { dispatch } = useStudioTasksContext();
+
+  const handleDeleteTask = async (id) => {
+    dispatch({ type: 'DELETE_STUDIOTASK', payload: task });
+    closeModal();
+    await deleteTask(id);
+  };
+
+  const handleArchiveTask = async (id) => {
+    dispatch({ type: 'DELETE_STUDIOTASK', payload: task });
+    closeModal();
+    await archiveStudioTask(id);
+  };
+
+  const handleFormChange = (e, key) => {
+    setFormValue((prev) => ({
+      ...prev,
+      [key]: e.target.value,
+    }));
+  };
+
+  const handleBlur = async () => {
+    setIsEditing(false);
+    try {
+      await UpdateStudioTask({ id: task._id, studioTaskData: formValue });
+      const res = await getAllStudioTasks();
+      dispatch({ type: 'SET_STUDIOTASKS', payload: res });
+    } catch (error) {
+      console.error('Error saving value:', error);
+    }
+  };
+
+  const handleMemberLoadChange = (name, loadState, loadPlace) => {
+    setIsMemberChangeLoading(() => {
+      return {
+        userName: name,
+        isLoading: loadState,
+        loadPlace,
+      };
+    });
+  };
+
+  const handleAddMember = async (userId: string, loadPlace: string) => {
+    const userToAdd = users.find((user) => user._id === userId);
+    const userToAddNasme = userToAdd.name;
+
+    if (!userToAdd) return;
+
+    const participants = [...task.participants];
+
+    const isAlreadyAdded = participants.some(
+      (participant) => participant._id === userId
+    );
+
+    if (isAlreadyAdded) return;
+
+    const newFormValue = {
+      ...task,
+      participants: [...participants, userToAdd],
+    };
+
+    setFormValue(newFormValue);
+
+    try {
+      handleMemberLoadChange(userToAddNasme, true, loadPlace);
+      await UpdateStudioTask({
+        id: task._id,
+        studioTaskData: newFormValue,
+      });
+      const res = await getAllStudioTasks();
+      dispatch({ type: 'SET_STUDIOTASKS', payload: res });
+    } catch (error) {
+      console.error('Error saving value:', error);
+    } finally {
+      handleMemberLoadChange(userToAddNasme, false, loadPlace);
+    }
+  };
+
+  const handleDeleteMember = async (userId: string, loadPlace: string) => {
+    const userToDelete = users.find((user) => user._id === userId);
+    const userToDeleteName = userToDelete.name;
+    const participants = [...task.participants];
+
+    const filteredParticipants = participants.filter(
+      (participant) => participant._id !== userId
+    );
+
+    setFormValue((prev) => {
+      return {
+        ...prev,
+        participants: filteredParticipants,
+      };
+    });
+
+    try {
+      handleMemberLoadChange(userToDeleteName, true, loadPlace);
+
+      await UpdateStudioTask({
+        id: task._id,
+        studioTaskData: { ...task, participants: filteredParticipants },
+      });
+      const res = await getAllStudioTasks();
+      dispatch({ type: 'SET_STUDIOTASKS', payload: res });
+    } catch (error) {
+      console.error('Error saving value:', error);
+    } finally {
+      handleMemberLoadChange(userToDeleteName, false, loadPlace);
+    }
+  };
+
+  const handleClientChange = async (e) => {
+    const companyObject = companies.filter(
+      (com) => com.name === e.target.value
+    );
+    const companyFirstClientPerson = companyObject[0].clientPerson[0].value;
+    handleFormChange(e, 'client');
+    setFormValue((prev) => {
+      return {
+        ...prev,
+        clientPerson: companyFirstClientPerson,
+      };
+    });
+    try {
+      await UpdateStudioTask({
+        id: task._id,
+        studioTaskData: {
+          ...task,
+          client: e.target.value,
+          clientPerson: companyFirstClientPerson,
+        },
+      });
+      const res = await getAllStudioTasks();
+      dispatch({ type: 'SET_STUDIOTASKS', payload: res });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const handleClientPersonChange = async (e) => {
+    handleFormChange(e, 'clientPerson');
+    try {
+      await UpdateStudioTask({
+        id: task._id,
+        studioTaskData: {
+          ...task,
+          clientPerson: e.target.value,
+        },
+      });
+      const res = await getAllStudioTasks();
+      dispatch({ type: 'SET_STUDIOTASKS', payload: res });
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const checkIfUserAssigned = (participantsUsers, userId) => {
+    return participantsUsers.some((participant) => participant._id === userId);
+  };
+  return {
+    users,
+    companies,
+    isEditing,
+    setIsEditing,
+    isSelectOpen,
+    setIsSelectOpen,
+    isMemberChangeLoading,
+    formValue,
+    handleFormChange,
+    handleDeleteTask,
+    handleArchiveTask,
+    handleBlur,
+    handleAddMember,
+    handleDeleteMember,
+    handleClientChange,
+    handleClientPersonChange,
+    checkIfUserAssigned,
+    isUserAssigned,
+    setIsUserAssigned,
+  };
+};
+
+export default useStudioTaskUpdate;
---
+++ frontend/src/hooks/useSubtasks.ts
@@ -0,0 +1,96 @@
+import { useState } from 'react';
+import {
+  addSubtask,
+  deleteSubtask,
+  updateSubtask,
+} from '../services/studio-tasks-service';
+import useStudioTasksContext from './Context/useStudioTasksContext';
+
+const useSubtask = (task) => {
+  const { dispatch } = useStudioTasksContext();
+
+  const [addSubtaskInput, setAddSubtaskInput] = useState({
+    isInputOpen: false,
+    isSubtaskLoading: false,
+    inputValue: '',
+  });
+  const [editSubtaskContent, setEditSubtaskContent] = useState({
+    isEditing: false,
+    isLoading: false,
+    contentValue: '',
+    subtaskId: '',
+  });
+
+  const handleAddSubtaskInput = (object) => {
+    setAddSubtaskInput((prev) => {
+      return {
+        ...prev,
+        ...object,
+      };
+    });
+  };
+
+  const handleEditSubtask = (object) => {
+    setEditSubtaskContent((prev) => {
+      return {
+        ...prev,
+        ...object,
+      };
+    });
+  };
+
+  const handleUpdateSubtask = async (taskId, subtaskId, subtaskData) => {
+    try {
+      handleEditSubtask({ isLoading: true, subtaskId });
+      const response = await updateSubtask({ taskId, subtaskId, subtaskData });
+      dispatch({ type: 'UPDATE_SUBTASK', payload: response });
+    } catch (error) {
+      console.error('Error saving value:', error);
+    } finally {
+      handleEditSubtask({ isLoading: false, subtaskId: '' });
+    }
+  };
+
+  const handleAddSubtask = async () => {
+    try {
+      if (addSubtaskInput.inputValue.length > 0) {
+        handleAddSubtaskInput({ isSubtaskLoading: true });
+
+        const response = await addSubtask({
+          taskId: task._id,
+          content: addSubtaskInput.inputValue,
+          done: false,
+        });
+        dispatch({ type: 'UPDATE_SUBTASK', payload: response });
+      }
+    } catch (error) {
+      console.error('Error saving value:', error);
+    } finally {
+      handleAddSubtaskInput({
+        isSubtaskLoading: false,
+        inputValue: '',
+        isInputOpen: false,
+      });
+    }
+  };
+
+  const handleDeleteSubtask = async (taskId, subtaskId) => {
+    try {
+      const response = await deleteSubtask(taskId, subtaskId);
+      dispatch({ type: 'UPDATE_SUBTASK', payload: response });
+    } catch (error) {
+      console.error('Error saving value:', error);
+    }
+  };
+  return {
+    handleAddSubtask,
+    handleDeleteSubtask,
+    handleAddSubtaskInput,
+    handleUpdateSubtask,
+    handleEditSubtask,
+    editSubtaskContent,
+    addSubtaskInput,
+  };
+};
+
+export default useSubtask;
