2b949d11940741ecbfbfe46885d8c1c0a95341c0
Davcode1234 <dpietruszewski12@gmail.com>
2025-01-29 14:17:23 +0100

    Comment out old task router to avoid bug

--- backend/src/Tasks/Task.router.ts
+++ backend/src/Tasks/Task.router.ts
@@ -3,7 +3,7 @@ import passport from 'passport';
 import { StatusCodes } from 'http-status-codes';
 import { TaskController } from './Task.controller';
 import '../Auth/Passport';
-import { uploadImg } from '../lib/firebase';
+// import { uploadImg } from '../lib/firebase';
 // import { multer } from '../lib/multer';
 
 // const upload = multer({ dest: 'uploads/' });
@@ -71,97 +71,97 @@ TaskRouter.get(
 //   },
 // );
 
-TaskRouter.post(
-  '/',
-  passport.authenticate('jwt', { session: false }),
-  // upload.single('image'),
-  async (req, res) => {
-    try {
-      const fileName = `${Date.now()}_${req.file.originalname}`;
-      const imageUrl = await uploadImg(req.file.buffer, fileName);
+// TaskRouter.post(
+//   '/',
+//   passport.authenticate('jwt', { session: false }),
+//   upload.single('image'),
+//   async (req, res) => {
+//     try {
+//       const fileName = `${Date.now()}_${req.file.originalname}`;
+//       const imageUrl = await uploadImg(req.file.buffer, fileName);
 
-      const newPost = await TaskController.addTask({
-        title: req.body.title,
-        author: req.user.id,
-        authorName: req.user.name,
-        // @ts-ignore
-        authorAvatar: req.user.img,
-        client: req.body.client,
-        path: req.body.path,
-        description: req.body.description,
-        image: imageUrl,
-        date: new Date(),
-        priority: req.body.priority,
-        status: req.body.status,
-        deadline: req.body.deadline,
-      });
-      res.status(StatusCodes.ACCEPTED).json(newPost);
-    } catch (error) {
-      console.error('Error during task creation:', error);
-      if (error.name === 'ValidationError') {
-        res.status(StatusCodes.BAD_REQUEST).json({ message: error.message });
-      } else {
-        res.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
-      }
-    }
-  },
-);
+//       const newPost = await TaskController.addTask({
+//         title: req.body.title,
+//         author: req.user.id,
+//         authorName: req.user.name,
+//         // @ts-ignore
+//         authorAvatar: req.user.img,
+//         client: req.body.client,
+//         path: req.body.path,
+//         description: req.body.description,
+//         image: imageUrl,
+//         date: new Date(),
+//         priority: req.body.priority,
+//         status: req.body.status,
+//         deadline: req.body.deadline,
+//       });
+//       res.status(StatusCodes.ACCEPTED).json(newPost);
+//     } catch (error) {
+//       console.error('Error during task creation:', error);
+//       if (error.name === 'ValidationError') {
+//         res.status(StatusCodes.BAD_REQUEST).json({ message: error.message });
+//       } else {
+//         res.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
+//       }
+//     }
+//   },
+// );
 
-TaskRouter.patch(
-  '/:id',
-  passport.authenticate('jwt', { session: false }),
-  async (req, res) => {
-    try {
-      const updatedTask = await TaskController.updateTask(req.params.id, {
-        ...req.body,
-      });
-      res.status(StatusCodes.ACCEPTED).json(updatedTask);
-    } catch (error) {
-      res.status(StatusCodes.NOT_FOUND).json({ message: error.message });
-    }
-  },
-);
+// TaskRouter.patch(
+//   '/:id',
+//   passport.authenticate('jwt', { session: false }),
+//   async (req, res) => {
+//     try {
+//       const updatedTask = await TaskController.updateTask(req.params.id, {
+//         ...req.body,
+//       });
+//       res.status(StatusCodes.ACCEPTED).json(updatedTask);
+//     } catch (error) {
+//       res.status(StatusCodes.NOT_FOUND).json({ message: error.message });
+//     }
+//   },
+// );
 
-TaskRouter.post(
-  '/:id/subtasks',
-  passport.authenticate('jwt', { session: false }),
-  async (req, res) => {
-    try {
-      const newSubtask = await TaskController.addSubtask(req.params.id, {
-        content: req.body.content,
-      });
-      res.status(StatusCodes.ACCEPTED).json(newSubtask);
-    } catch (error) {
-      res.status(StatusCodes.BAD_REQUEST).json({ message: error.message });
-    }
-  },
-);
+// TaskRouter.post(
+//   '/:id/subtasks',
+//   passport.authenticate('jwt', { session: false }),
+//   async (req, res) => {
+//     try {
+//       const newSubtask = await TaskController.addSubtask(req.params.id, {
+//         content: req.body.content,
+//       });
+//       res.status(StatusCodes.ACCEPTED).json(newSubtask);
+//     } catch (error) {
+//       res.status(StatusCodes.BAD_REQUEST).json({ message: error.message });
+//     }
+//   },
+// );
 
-TaskRouter.delete(
-  '/:taskID/subtasks/:subtaskID',
-  passport.authenticate('jwt', { session: false }),
-  async (req, res) => {
-    try {
-      await TaskController.deleteSubtask(
-        req.params.taskID,
-        req.params.subtaskID,
-      );
-      res.status(StatusCodes.NO_CONTENT).json({});
-    } catch (error) {
-      res.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
-    }
-  },
-);
+// TaskRouter.delete(
+//   '/:taskID/subtasks/:subtaskID',
+//   passport.authenticate('jwt', { session: false }),
+//   async (req, res) => {
+//     try {
+//       await TaskController.deleteSubtask(
+//         req.params.taskID,
+//         req.params.subtaskID,
+//       );
+//       res.status(StatusCodes.NO_CONTENT).json({});
+//     } catch (error) {
+//       res.status(StatusCodes.INTERNAL_SERVER_ERROR).send();
+//     }
+//   },
+// );
 
-TaskRouter.delete(
-  '/:id',
-  passport.authenticate('jwt', { session: false }),
-  async (req, res) => {
-    try {
-      const task = await TaskController.deleteTask(req.params.id);
-      res.status(StatusCodes.NO_CONTENT).json(task);
-    } catch (error) {
-      res.status(StatusCodes.NOT_FOUND).json({ message: error.message });
-    }
-  },
-);
+// TaskRouter.delete(
+//   '/:id',
+//   passport.authenticate('jwt', { session: false }),
+//   async (req, res) => {
+//     try {
+//       const task = await TaskController.deleteTask(req.params.id);
+//       res.status(StatusCodes.NO_CONTENT).json(task);
+//     } catch (error) {
+//       res.status(StatusCodes.NOT_FOUND).json({ message: error.message });
+//     }
+//   },
+// );
