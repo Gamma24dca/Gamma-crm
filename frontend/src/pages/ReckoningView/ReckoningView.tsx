67ad9d9e5f47875436062277208beff295649ca5
Davcode1234 <dpietruszewski12@gmail.com>
2025-02-10 15:30:27 +0100

    Start styling reckoning view

--- frontend/src/pages/ReckoningView/ReckoningView.tsx
+++ frontend/src/pages/ReckoningView/ReckoningView.tsx
@@ -1,24 +1,149 @@
+import { useEffect, useState } from 'react';
 import ControlBar from '../../components/Atoms/ControlBar/ControlBar';
 import ControlBarTitle from '../../components/Atoms/ControlBar/Title/ControlBarTitle';
 import InfoBar from '../../components/Atoms/InfoBar/InfoBar';
 import ListContainer from '../../components/Atoms/ListContainer/ListContainer';
+import Select from '../../components/Atoms/Select/Select';
 import ViewContainer from '../../components/Atoms/ViewContainer/ViewContainer';
+import useCurrentDate from '../../hooks/useCurrentDate';
+import styles from './ReckoningView.module.css';
+
+function generateDaysArray(month, year) {
+  const daysInMonth = new Date(year, month, 0).getDate(); // Get number of days in the given month
+  const daysArray = [];
+
+  for (let i = 1; i <= daysInMonth; i += 1) {
+    daysArray.push({ hourNum: 0 }); // Assign a random value (0-19)
+  }
+
+  return daysArray;
+}
+
+const mockedTasks = [
+  {
+    _id: 'ig35c',
+    worker: 'Bartek',
+    month: 'marzec',
+    company: 'Aksil',
+    createdAt: '2024.03.03',
+    client: 'Stachowiczk Joanna',
+    taskTitle: 'AKSIL_KATALOG_PRODUKTOW 2024',
+    hours: generateDaysArray(2, 2025),
+    comment: 'projekt, poprawki i pliki do druku',
+    printWhere: 'Zetka',
+    printSpec: 'A4',
+    isSettled: true,
+  },
+  {
+    _id: 'fps32',
+    worker: 'Edyta',
+    month: 'wrzesień',
+    company: 'Santander',
+    createdAt: '2024.05.01',
+    client: 'Badowska Alicja',
+    taskTitle: 'Baner industry 1070x2125',
+    hours: generateDaysArray(2, 2025),
+    comment: 'Zaliczka 12',
+    printWhere: 'RM Print',
+    printSpec: 'A5',
+    isSettled: false,
+  },
+  {
+    _id: '3x8[2',
+    worker: 'Edyta',
+    month: 'kwiecień',
+    company: 'Santander',
+    createdAt: '2024.04.08',
+    client: 'Ożóg Joanna',
+    taskTitle: 'Avik Animation 4k',
+    hours: generateDaysArray(2, 2025),
+    comment: '',
+    printWhere: 'Zetka',
+    printSpec: 'A4',
+    isSettled: true,
+  },
+];
 
 function StudioTaskView() {
+  const {
+    selectedMonth,
+    selectedYear,
+    handleMonthChange,
+    handleYearChange,
+    months,
+    years,
+  } = useCurrentDate();
+
+  const [selectedMonthDaysArray, setSelectedMonthDaysArray] = useState([]);
+
+  useEffect(() => {
+    const monthIndex = months.indexOf(selectedMonth) + 1;
+
+    setSelectedMonthDaysArray(generateDaysArray(monthIndex, 2025));
+  }, [selectedMonth]);
   return (
     <>
       <ControlBar>
         <ControlBarTitle>Roliczenie</ControlBarTitle>
+        <Select
+          value={selectedMonth}
+          handleValueChange={handleMonthChange}
+          optionData={months}
+        />
+
+        <Select
+          value={selectedYear}
+          handleValueChange={handleYearChange}
+          optionData={years}
+        />
       </ControlBar>
       <ViewContainer>
         <ListContainer>
           <InfoBar>
-            <div>1</div>
-            <div>2</div>
-            <div>3</div>
-            <div>4</div>
-            <div>5</div>
+            <div className={styles.infoBarContainer}>
+              <p>Firma</p>
+              <p>Klient</p>
+              <p>Tytuł</p>
+              <p>Komentarz</p>
+              <div className={styles.daysWrapper}>
+                {selectedMonthDaysArray.map((dayTile, index) => {
+                  return (
+                    <p className={styles.dayNumberInfo} key={index}>
+                      {index + 1}
+                    </p>
+                  );
+                })}
+              </div>
+            </div>
           </InfoBar>
+
+          <div className={styles.reckoningContainer}>
+            {mockedTasks.map((reckTask) => {
+              return (
+                <div
+                  className={styles.reckoningItemContainer}
+                  key={reckTask._id}
+                >
+                  <div>{reckTask.company}</div>
+                  <div>{reckTask.client}</div>
+                  <div>{reckTask.taskTitle}</div>
+                  <div>{reckTask.comment}</div>
+                  <div>{reckTask.printSpec}</div>
+                  <div>{reckTask.printWhere}</div>
+
+                  <div className={styles.daysWrapper}>
+                    {selectedMonthDaysArray.map((dayTile, index) => {
+                      return (
+                        <div className={styles.dayTile} key={index}>
+                          {dayTile.hourNum > 0 && dayTile.hourNum}
+                        </div>
+                      );
+                    })}
+                  </div>
+                </div>
+              );
+            })}
+          </div>
         </ListContainer>
       </ViewContainer>
     </>
