df3d4adced3f19fad38f02f89918c675fe09b99f
Davcode1234 <dpietruszewski12@gmail.com>
2025-02-12 15:54:07 +0100

    some adjustments

--- frontend/src/components/Templates/MainTemplate/MainTemplate.tsx
+++ frontend/src/components/Templates/MainTemplate/MainTemplate.tsx
@@ -2,21 +2,12 @@ import Navigation from '../../Organisms/Navigation/Navigation';
 import SideNavigation from '../../Organisms/SideNavigation/SideNavigation';
 import { ChildrenProps } from '../../../providers/UserProvider';
 import styles from './MainTemplate.module.css';
-import useSideNavHide from '../../../hooks/useSideNavHide';
 
 function MainTemplate({ children }: ChildrenProps) {
-  const { isSideNavHidden } = useSideNavHide();
-
   return (
     <div className={styles.mainWrapper}>
       <SideNavigation />
-      <div
-        className={`${
-          !isSideNavHidden
-            ? styles.restContentWrapper
-            : styles.fullSizeRestContentWrapper
-        }`}
-      >
+      <div className={styles.restContentWrapper}>
         <Navigation />
         {children}
       </div>
