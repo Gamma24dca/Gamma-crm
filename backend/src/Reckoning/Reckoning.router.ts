0d04bc0467fea7e1bd1173b796bd9e64010db138
Davcode1234 <dpietruszewski12@gmail.com>
2025-02-14 08:57:20 +0100

    Finish reckoning tasks api

--- backend/src/Reckoning/Reckoning.router.ts
+++ backend/src/Reckoning/Reckoning.router.ts
@@ -35,3 +35,61 @@ ReckoningTaskRouter.get(
     }
   },
 );
+
+ReckoningTaskRouter.post(
+  '/',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    try {
+      const newStudioTask = await ReckoningTaskController.addReckoningTask({
+        searchID: req.body.searchID,
+        client: req.body.client,
+        clientPerson: req.body.clientPerson,
+        title: req.body.title,
+        description: req.body.description,
+        author: req.body.author,
+        taskType: req.body.taskType,
+        printWhat: req.body.printWhat,
+        printWhere: req.body.printWhere,
+        participants: req.body.participants,
+        startDate: req.body.startDate,
+        deadline: req.body.deadline,
+      });
+      res.status(StatusCodes.CREATED).json(newStudioTask);
+    } catch (error) {
+      console.error(error);
+      res.status(StatusCodes.BAD_REQUEST).json({ message: error });
+    }
+  },
+);
+
+ReckoningTaskRouter.patch(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    try {
+      const updatedReckoningTask =
+        await ReckoningTaskController.updateReckoningTask(req.params.id, {
+          ...req.body,
+        });
+      res.status(StatusCodes.ACCEPTED).json(updatedReckoningTask);
+    } catch (error) {
+      console.error(error);
+      res.status(StatusCodes.BAD_REQUEST).json({ message: error });
+    }
+  },
+);
+
+ReckoningTaskRouter.delete(
+  '/:id',
+  passport.authenticate('jwt', { session: false }),
+  async (req, res) => {
+    try {
+      await ReckoningTaskController.deleteReckoningTask(req.params.id);
+      res.status(StatusCodes.ACCEPTED);
+    } catch (error) {
+      console.error(error);
+      res.status(StatusCodes.BAD_REQUEST).json({ message: error });
+    }
+  },
+);
