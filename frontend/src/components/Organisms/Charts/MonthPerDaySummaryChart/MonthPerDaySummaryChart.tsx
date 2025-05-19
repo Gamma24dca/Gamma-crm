87114eaa61d634a853db063bc7eb44cfe09c57d8
Davcode1234 <dpietruszewski12@gmail.com>
2025-05-19 15:38:11 +0200

    add data ready to other charts and view select

--- frontend/src/components/Organisms/Charts/MonthPerDaySummaryChart/MonthPerDaySummaryChart.tsx
+++ frontend/src/components/Organisms/Charts/MonthPerDaySummaryChart/MonthPerDaySummaryChart.tsx
@@ -8,38 +8,52 @@ import {
   XAxis,
   YAxis,
 } from 'recharts';
+import { Icon } from '@iconify/react';
 import DashboardContainerTitle from '../../../Atoms/DashboardContainerTitle/DashboardContainerTitle';
 import styles from './MonthPerDaySummaryChart.module.css';
 
-function MonthPerDaySummaryChart({ selectedMonth, monthDaysSummary }) {
+function MonthPerDaySummaryChart({
+  selectedMonth,
+  monthDaysSummary,
+  dataReady,
+}) {
   return (
     <div className={styles.lineChartContainer}>
-      <DashboardContainerTitle>{`Podsumowanie miesiąca - ${selectedMonth}`}</DashboardContainerTitle>
-      <ResponsiveContainer width="100%" height="85%">
-        <LineChart
-          width={500}
-          height={300}
-          data={monthDaysSummary}
-          margin={{
-            top: 5,
-            right: 30,
-            left: 20,
-            bottom: 5,
-          }}
-        >
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="day" />
-          <YAxis />
-          <Tooltip />
-          <Legend />
-          <Line
-            type="monotone"
-            dataKey="totalHours"
-            stroke="#8884d8"
-            activeDot={{ r: 8 }}
-          />
-        </LineChart>
-      </ResponsiveContainer>
+      <DashboardContainerTitle>{`[h] Podsumowanie miesiąca - ${selectedMonth}`}</DashboardContainerTitle>
+      {dataReady ? (
+        <ResponsiveContainer width="100%" height="85%">
+          <LineChart
+            width={500}
+            height={300}
+            data={monthDaysSummary}
+            margin={{
+              top: 5,
+              right: 30,
+              left: 20,
+              bottom: 5,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="day" />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Line
+              type="monotone"
+              dataKey="totalHours"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+              animationBegin={0}
+              animationDuration={2000}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      ) : (
+        <div className={styles.noTasksContainer}>
+          <p>Brak danych</p>
+          <Icon icon="line-md:coffee-loop" width="24" height="24" />
+        </div>
+      )}
     </div>
   );
 }
--- frontend/src/components/Organisms/Charts/TypesRadarChart/TypesRadarChart.tsx
+++ frontend/src/components/Organisms/Charts/TypesRadarChart/TypesRadarChart.tsx
@@ -7,28 +7,41 @@ import {
   RadarChart,
   ResponsiveContainer,
 } from 'recharts';
+import { Icon } from '@iconify/react';
 import DashboardContainerTitle from '../../../Atoms/DashboardContainerTitle/DashboardContainerTitle';
 import styles from './TypesRadarChart.module.css';
 
-function TypesRadarChart({ tasksTypeSummary }) {
+function TypesRadarChart({ tasksTypeSummary, dataReady }) {
   return (
     <div className={styles.radarChartContainer}>
       <DashboardContainerTitle>Typy aktywnych zleceń</DashboardContainerTitle>
-      <ResponsiveContainer width="100%" height="85%">
-        <RadarChart cx="50%" cy="50%" outerRadius="80%" data={tasksTypeSummary}>
-          <Label value="any" />
-          <PolarGrid />
-          <PolarAngleAxis dataKey="taskType" />
-          <PolarRadiusAxis />
-          <Radar
-            name="Mike"
-            dataKey="count"
-            stroke="#8884d8"
-            fill="#8884d8"
-            fillOpacity={0.6}
-          />
-        </RadarChart>
-      </ResponsiveContainer>{' '}
+      {dataReady ? (
+        <ResponsiveContainer width="100%" height="85%">
+          <RadarChart
+            cx="50%"
+            cy="50%"
+            outerRadius="80%"
+            data={tasksTypeSummary}
+          >
+            <Label value="any" />
+            <PolarGrid />
+            <PolarAngleAxis dataKey="taskType" />
+            <PolarRadiusAxis />
+            <Radar
+              name="Mike"
+              dataKey="count"
+              stroke="#8884d8"
+              fill="#8884d8"
+              fillOpacity={0.6}
+            />
+          </RadarChart>
+        </ResponsiveContainer>
+      ) : (
+        <div className={styles.noTasksContainer}>
+          <p>Brak danych</p>
+          <Icon icon="line-md:coffee-loop" width="24" height="24" />
+        </div>
+      )}
     </div>
   );
 }
