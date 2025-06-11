e779c21af7bb1d9325cd5f357b52166d858034a1
Davcode1234 <dpietruszewski12@gmail.com>
2025-06-11 08:26:37 +0200

    Add clients dropdown to edit company modal

--- frontend/src/services/clients-service.ts
+++ frontend/src/services/clients-service.ts
@@ -59,6 +59,31 @@ export async function getCurrentClient(
   }
 }
 
+export async function getClientsByCompany(company: string) {
+  try {
+    const response = await fetch(
+      `${import.meta.env.VITE_API_URL}/api/clients/bycompany/${company}`,
+      {
+        method: 'GET',
+        credentials: 'include',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    if (response.ok) {
+      return await response.json();
+    }
+    throw new Error(`${response.status} ${response.statusText}`);
+  } catch (error) {
+    if (Config.isDev) {
+      throw new Error('Get Client', error.message);
+    }
+    return null;
+  }
+}
+
 export async function addClient({ name, company, phone, email }) {
   const formData = {
     name,
